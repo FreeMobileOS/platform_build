440524fe137d802652c7ad97287a4caaf0b30824
Satish Patel <tosatishpatel@gmail.com>
2017-09-13 18:15:52 +0530

    Add wire app to build
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- target/product/core.mk
+++ target/product/core.mk
@@ -67,7 +67,8 @@ PRODUCT_PACKAGES += \
     signal \
     vlc \
     mintube \
-    PassAndroid
+    PassAndroid \
+    wire
 
 # The set of packages whose code can be loaded by the system server.
 PRODUCT_SYSTEM_SERVER_APPS += \

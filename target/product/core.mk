c6e696e459ad5cbfb718f99e4511c2073aabfdb4
Satish Patel <tosatishpatel@gmail.com>
2017-10-23 11:53:36 +0530

    Add openlauncher
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- target/product/core.mk
+++ target/product/core.mk
@@ -74,7 +74,8 @@ PRODUCT_PACKAGES += \
     silence \
     yalpstore \
     opencamera \
-    kisslauncher
+    kisslauncher \
+    openlauncher
 
 # The set of packages whose code can be loaded by the system server.
 PRODUCT_SYSTEM_SERVER_APPS += \
